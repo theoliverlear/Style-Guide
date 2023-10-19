2e788379bd715d11ee7920d6b841e926dfeae47e
theoliverlear <sigwartholiver2@gmail.com>
2023-10-19 08:15:13 -0500

    Rename Scanner
    Took 18 minutes

--- Examples/Example1_1.java
+++ Examples/Example1_1.java
@@ -3,18 +3,18 @@ import java.util.InputMismatchException; // Import the InputMismatchException cl
 import java.util.Scanner; // Import the Scanner class
 public class Example1_1 { // Begin class
     public static void main(String[] args) { // Begin main method
-        Scanner input = new Scanner(System.in); // Create a Scanner object
+        Scanner userInput = new Scanner(System.in); // Create a Scanner object
         boolean continueInput = true; // Create a boolean variable
         do { // Begin do-while loop
             try { // Begin try block
                 System.out.println("Enter an integer: "); // Print a message
-                int number = input.nextInt(); // Input an integer
+                int number = userInput.nextInt(); // Input an integer
                 System.out.println("The number entered is " + number); // Print a message
                 continueInput = false; // Exit the loop
             } catch (InputMismatchException ex) { // Begin catch block
                 System.out.println("Try again. (Incorrect input: an integer" +
-                               " is" + " required)"); // Print a message
-                input.nextLine(); // Clear input
+                               " is required)"); // Print a message
+                userInput.nextLine(); // Clear input
             } // End catch block
         } while (continueInput); // End do-while loop
     } // End main method
--- Examples/Example1_2.java
+++ Examples/Example1_2.java
@@ -3,18 +3,18 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Example1_2 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);             // Read user input
+        Scanner userInput = new Scanner(System.in);         // Read user input
         boolean continueInput = true;
         do {
             try {
                 System.out.println("Enter an integer: ");
-                int number = input.nextInt();              // Input an integer
+                int number = userInput.nextInt();          // Input an integer
                 System.out.println("The number entered is " + number);
                 continueInput = false;                        // Exit the loop
             } catch (InputMismatchException ex) {
                 System.out.println("Try again. (Incorrect input: an integer" +
-                                   " is" + " required)");
-                input.nextLine();                               // Clear input
+                                   " is required)");
+                userInput.nextLine();                           // Clear input
             }
         } while (continueInput);
     }
